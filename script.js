23162f5d0508cad621b66793b69dd7f2cf341def
Aldo <latifi.antonis@gmail.com>
2023-10-28 14:46:18 +0100

    Added the container div

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const container = document.getElementById('container');
+const rows = 16;
+const cols = 16;
+function createGrid() {
+  for (let i=0;i<rows; i++) {
+      for (let j=0;j<cols; j++) {
+        const gridItem = document.createElement('div');
+        gridItem.className = 'grid-item';
+        container.appendChild(gridItem);
+      }
+   }
+}  
 
+createGrid();
