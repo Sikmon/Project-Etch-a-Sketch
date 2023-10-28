6261274d1d60cf962997c2f6ed62d45a1c6a904f
Aldo <latifi.antonis@gmail.com>
2023-10-28 15:23:14 +0100

    Added the mouseover effect on the grid and a clear button to clear the grid from the black color

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const container = document.getElementById('container');
+const clearBtn = document.getElementById('clr');
 const rows = 16;
 const cols = 16;
 function createGrid() {
   for (let i=0;i<rows; i++) {
       for (let j=0;j<cols; j++) {
         const gridItem = document.createElement('div');
-        gridItem.className = 'grid-item';
+        gridItem.className = 'gridItem';
+        gridItem.addEventListener('mouseover', () => {
+            gridItem.style.backgroundColor = 'black';
+        });
         container.appendChild(gridItem);
+        
       }
    }
+   
 }  
 
 createGrid();
+
+function clearGrid() {
+    const items = document.querySelectorAll('.gridItem');
+    items.forEach(item =>{
+        item.style.backgroundColor = 'white';
+    })
+}
+
+clearBtn.addEventListener('click', clearGrid);
